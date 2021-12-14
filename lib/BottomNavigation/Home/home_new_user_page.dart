ff437ee41b243e0542415221eb21ae7ce5b7a3c4
Hassan Ayoub Zafar <60160952+hassan-zafar@users.noreply.github.com>
2021-12-15 00:30:11 +0500

    Create home_new_user_page.dart

---
+++ lib/BottomNavigation/Home/home_new_user_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomeNewUserFollowPage extends StatefulWidget {
+  const HomeNewUserFollowPage({ Key? key }) : super(key: key);
+
+  @override
+  _HomeNewUserFollowPageState createState() => _HomeNewUserFollowPageState();
+}
+
+class _HomeNewUserFollowPageState extends State<HomeNewUserFollowPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}

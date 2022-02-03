fbac2078dad994cc35bff64706fb54bb1b6c8bba
Hassan Ayoub Zafar <60160952+hassan-zafar@users.noreply.github.com>
2022-02-04 03:45:17 +0500

    Update collections.dart

--- lib/Constants/collections.dart
+++ lib/Constants/collections.dart
@@ -5,3 +5,5 @@ final commentsRef = FirebaseFirestore.instance.collection('comments');
 final chatRoomRef = FirebaseFirestore.instance.collection('chatRoom');
 final chatListRef = FirebaseFirestore.instance.collection('chatLists');
 final activityFeedsRef = FirebaseFirestore.instance.collection('activityFeeds');
+final followerRef = FirebaseFirestore.instance.collection('followerRef');
+final followingRef = FirebaseFirestore.instance.collection('followingRef');
